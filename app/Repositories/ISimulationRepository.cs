dc3a306bb2cda1ccb57b8a0bd51b8aa489dafb67
Richard Lopes <contato.richardlopes@outlook.com>
2023-11-10 16:36:01 -0300

    feat: add dashboard queries on simulation repository

--- app/Repositories/ISimulationRepository.cs
+++ app/Repositories/ISimulationRepository.cs
@@ -4,5 +4,9 @@ namespace App.Repositories
 {
     public interface ISimulationRepository : IRepository<Simulation>
     {
+        Task<IDictionary<Institution, int>> CountByInstitution(int year);
+        Task<IDictionary<Course, int>> CountByCourse(int year);
+        Task<IDictionary<Institution, int>> CountConversionsByInstitution(int year);
+        Task<IDictionary<Course, int>> CountConversionsByCourse(int year);
     }
 }
